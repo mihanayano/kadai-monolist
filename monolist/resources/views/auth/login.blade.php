bc2499e67c55c9c0b8e44e9695217149c14e5ce4
EC2 Default User <ec2-user@ip-172-31-5-153.us-east-2.compute.internal>
2018-06-15 07:54:19 +0000

    user wlogin

--- monolist/resources/views/auth/login.blade.php
+++ monolist/resources/views/auth/login.blade.php
@@ -1,49 +1,28 @@
-<header>
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-left" href="/"><img src="{{ secure_asset("images/logo.png") }}" alt="Monolist"></a>
-            </div>
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    @if (Auth::check())
-                        <li>
-                            <a href="#">
-                                <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-                                アイテムを追加
-                              </a>
-                        </li>
+@extends('layouts.app')
 
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                                <span class="gravatar">
-                                    <img src="{{ Gravatar::src(Auth::user()->email, 20) . '&d=mm' }}" alt="" class="img-circle">
-                                </span>
-                                {{ Auth::user()->name }}
-                                <span class="caret"></span>
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <a href="#">マイページ</a>
-                                </li>
-                                <li role="separator" class="divider"></li>
-                                <li>
-                                    <a href="{{ route('logout.get') }}">ログアウト</a>
-                                </li>
-                            </ul>
-                        </li>
-                    @else
-                        <li><a href="{{ route('signup.get') }}">新規登録</a></li>
-                        <li><a href="{{ route('login') }}">ログイン</a></li>
-                    @endif
-                </ul>
+@section('content')
+<div class="row">
+    <div class="col-xs-offset-3 col-xs-6">
+        <div class="panel panel-default">
+            <div class="panel-heading">ログイン</div>
+            <div class="panel-body">
+                {!! Form::open(['route' => 'login.post']) !!}
+                    <div class="form-group">
+                        {!! form::label('email', 'メールアドレス') !!}
+                        {!! form::email('email', old('email'), ['class' => 'form-control']) !!}
+                    </div>
+
+                    <div class="form-group">
+                        {!! form::label('password', 'パスワード') !!}
+                        {!! form::password('password', ['class' => 'form-control']) !!}
+                    </div>
+
+                    <div class="text-right">
+                        {!! form::submit('ログイン', ['class' => 'btn btn-success']) !!}
+                    </div>
+                {!! form::close() !!}
             </div>
         </div>
-    </nav>
-</header>
+    </div>
+</div>
+@endsection
